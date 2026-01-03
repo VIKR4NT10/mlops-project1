70233e2d03ef6d0d7e6c7a946f786e6a5e694776
Vikrant Chavan <vikrantschavan3@gmail.com>
2026-01-03 13:26:48 +0530

    ready till model pusher

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -42,3 +42,30 @@ def configure_logger():
 
 # Configure the logger
 configure_logger()
+# def configure_logger():
+#     logger = logging.getLogger()
+#     logger.setLevel(logging.DEBUG)
+
+#     # 🔥 Clear existing handlers
+#     if logger.hasHandlers():
+#         logger.handlers.clear()
+
+#     formatter = logging.Formatter(
+#         "[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s"
+#     )
+
+#     file_handler = RotatingFileHandler(
+#         log_file_path,
+#         maxBytes=MAX_LOG_SIZE,
+#         backupCount=BACKUP_COUNT
+#     )
+#     file_handler.setLevel(logging.DEBUG)
+#     file_handler.setFormatter(formatter)
+
+#     console_handler = logging.StreamHandler()
+#     console_handler.setLevel(logging.INFO)
+#     console_handler.setFormatter(formatter)
+
+#     logger.addHandler(file_handler)
+#     logger.addHandler(console_handler)
+# configure_logger()
